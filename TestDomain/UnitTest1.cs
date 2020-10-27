7a80ddb998457056073b5dc0f23730f9fbee7557
toribraun <torribraun161@gmail.com>
2020-10-27 13:58:35 +0500

    first test

---
+++ TestDomain/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace TestDomain
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
