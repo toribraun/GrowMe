8e52e60640e1db79cf29330a3e116e4679f2c9f2
toribraun <torribraun161@gmail.com>
2020-11-03 14:11:02 +0500

    Bot's first message

--- Infrastructure/Base.cs
+++ Infrastructure/Base.cs
@@ -1,5 +1,9 @@
 namespace Infrastructure
 {
+    public interface IDataBase
+    {
+        // void AddOrUpdateUser(User user);
+    }
     public class Base
     {
         
--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
-
-namespace UserInterface
+﻿namespace UserInterface
 {
+    using System;
+    using Telegram.Bot;
+    using Telegram.Bot.Args;
+    using Telegram.Bot.Types.Enums;
+
     class Program
     {
+        private static TelegramBotClient client;
+        private static string token = "1017290663:AAF1ZG3q_hGOZF5rCfJDh-WbT-NLgGGMW98";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            client = new TelegramBotClient(token);
+            client.OnMessage += BotOnMessageReceived;
+            client.StartReceiving();
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message?.Type == MessageType.Text)
+            {
+                var newMesage = $"Hello, {message.Chat.FirstName}!";
+                await client.SendTextMessageAsync(message.Chat.Id, newMesage);
+            }
         }
     }
 }
