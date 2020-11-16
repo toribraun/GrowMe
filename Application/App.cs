a68119ec35f41deeaa51af4d4b834e4c511043e0
Angy Blacwood <angy.blackwood@gmail.com>
2020-11-17 00:45:43 +0500

    Added App

---
+++ Application/App.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Infrastructure;
+
+namespace Application
+{
+    class App
+    {
+        private CsvDatabase database;
+
+        public App()
+        {
+            database = new CsvDatabase(
+                "users.csv", "users_plants.csv", "common_plants.csv");
+        }
+        
+        public bool AddUser(User user)
+        {
+            if (database.GetUsers().Contains(user))
+                return false;
+            database.AddUser(user);
+            return true;
+        }
+
+        public string GetPlantsByUser(User user)
+        {
+            return database.GetPlantsByUser(user)
+                .Aggregate("", (message, plant) => message + $"{plant.Name}\n");
+        }
+    }
+}
